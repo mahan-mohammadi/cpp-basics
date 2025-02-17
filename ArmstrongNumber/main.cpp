1bd34e42d9190deaeff709d4007e5e80889d2213
mahan <mahan.mohammadi.a@gmail.com>
2025-02-17 19:40:07 +0330

    armstrong number

---
+++ ArmstrongNumber/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main() {
+    int num,sum=0,i=0;
+    cout << "Please enter a number: ";
+    cin >> num;
+    int savedNum1=num;
+    int savedNum2=num;
+
+    while (savedNum1) {
+        sum += savedNum1%10 * (savedNum1%10) * (savedNum1%10) ;
+        savedNum1/=10;
+    }
+    if (sum == savedNum2) {
+        cout << num << " is a armstrong number" << endl;
+    }
+    else {
+        cout << num << " is not a armstrong number" << endl;
+    }
+
+    return 0;
+}
