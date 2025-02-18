5141aec9f6365cff901131cbea0c1572058fc9a8
mahan <mahan.mohammadi.a@gmail.com>
2025-02-18 20:42:12 +0330

    make it a harder question i guess?

--- Triangle/main.cpp
+++ Triangle/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int row;
+    int row,number=1;
     cin >> row;
     for (int i = 1; i <= row; i++) {
-        for (int j = 1; j <= i; j++) {
-            cout << i;
+        for (int j = 1; j <= i; j++,number++) {
+            cout << number << " ";
         }
         cout << endl;
     }
